f779a1dde93765ba0198466e9bb7786855f48e49
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-14 00:44:37 -0400

    fixed search query to include category name

--- I210-Final/searchitemresults.php
+++ I210-Final/searchitemresults.php
@@ -17,7 +17,13 @@ $delimiter = ' ';
 $terms = explode($delimiter, $user_terms);
 
 // defining the query
-$sql = "SELECT * FROM items WHERE item_id>0";
+$sql = "
+SELECT i.item_id, i.item_name, i.item_price, i.image, i.description, c.category_name 
+FROM items as i
+INNER JOIN categories as c
+ON i.category_id = c.category_id
+WHERE i.item_id>0
+";
 foreach ($terms as $term) {
     $sql .= " AND item_name LIKE '%$term%'";
 }
@@ -37,7 +43,8 @@ if (!$query) {
 
     //display results in a table
     if ($query->num_rows == 0)
-    echo "Your search <i>'$user_terms'</i> did not match any items in our inventory";
+    echo "<p style='font-size:30px; padding:50px'>Your search <strong>'$user_terms'</strong> did not match any items in our inventory.</p><br>";
+
     else {
     ?>
         <section class="menu-display">
@@ -49,6 +56,7 @@ if (!$query) {
                 echo "<img src='images/", $row['image'], "' alt='' />";
                 echo "<div class='food-text'>";
                 echo "<h2>", $row['item_name'], "</h2>";
+                echo "<h4>", $row['category_name'], "</h4>";
                 echo "<p>", $row['description'], "</p>";
                 echo "</div>";
                 echo "<a href='menu-details.php?id=", $row['item_id'], "'>View Details</a>";
