16f00628a54a5d07f87e41474ebecf4160a1a60e
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-14 03:30:13 -0400

    Got the highlighted search terms working

--- I210-Final/searchitemresults.php
+++ I210-Final/searchitemresults.php
@@ -43,19 +43,77 @@ if (!$query) {
 
     //display results in a table
     if ($query->num_rows == 0)
-    echo "<p style='font-size:30px; padding:50px'>Your search <strong>'$user_terms'</strong> did not match any items in our inventory.</p><br>";
-
+    echo "<p style='font-size:30px; padding:25px'>Your search <strong>'$user_terms'</strong> did not match any items in our inventory.</p><br>
+    <a href='searchitems.php' style='margin:25px' class='search-back'>Go Back</a>
+";
     else {
     ?>
+        <h1 id="menuHeader">Results for <strong>"<?= $user_terms ?>"</strong>:</h1>
         <section class="menu-display">
             <div class="food-wrapper">
         <?php
         //insert a food item for each row in the query
             while (($row = $query->fetch_assoc()) !== null) {
+                $item_name = $row['item_name'];
+                $highlighted_chars = [];
+
+                // looping through each term
+                foreach($terms as $term) {
+                    $start_pos = 0;
+                    $term_len = strlen($term);
+                    $term_ranges = [];
+
+                    // getting the ranges for all instances of the term
+                    while ($start_pos <= strlen($item_name)) {
+                        $string_pos = stripos($item_name, $term, $start_pos);
+                        if ($start_pos >= $string_pos and $string_pos != "") {
+                            $string_end_pos = $string_pos + $term_len - 1;
+                            $string_range = ($string_pos . ":" . $string_end_pos);
+                            array_push($term_ranges, $string_range);
+                        }
+                        $start_pos += 1;
+                    }
+
+
+                    $term_char_positions = [];
+
+                    // going through and getting the range values from $term_ranges and putting them into an array
+                    foreach($term_ranges as $range_str) {
+                        $range_vals = explode(":", $range_str);
+                        $start_val = $range_vals[0];
+                        $end_val = $range_vals[1];
+                        $range = range($start_val, $end_val);
+
+                        foreach ($range as $val){
+                            array_push($term_char_positions, $val);
+                        }
+                    }
+
+                    // putting the range values into a larger array, only if the position isn't already highlighted
+                    foreach($term_char_positions as $char_position) {
+                        if (!in_array($char_position, $highlighted_chars)) {
+                            array_push($highlighted_chars, $char_position);
+                        }
+                    }
+                }
+
+                // creating the new term with the highlighed background for the proper character
+                $term_str = str_split($item_name);
+                $new_name = '';
+                foreach($term_str as $key=>$term_char) {
+                    if (in_array($key, $highlighted_chars)) {
+                        $new_name .= "<span class='search-highlight'>$term_char</span>";
+                    } else {
+                        $new_name .= $term_char;
+                    }
+                }
+
                 echo "<div class='food'>";
                 echo "<img src='images/", $row['image'], "' alt='' />";
                 echo "<div class='food-text'>";
-                echo "<h2>", $row['item_name'], "</h2>";
+                echo "<h2>";
+                echo $new_name;
+                echo "</h2>";
                 echo "<h4>", $row['category_name'], "</h4>";
                 echo "<p>", $row['description'], "</p>";
                 echo "</div>";
--- I210-Final/searchitems.php
+++ I210-Final/searchitems.php
@@ -1,11 +1,11 @@
 <?php
 include ('includes/header.php');
 ?>
-    <h2>Search Items by Title</h2>
-    <p>Enter one or more words in item title.</p>
+    <h2 style="margin-top: 25px">Search Items by Title</h2>
+    <p style="margin-bottom: 15px">Enter one or more words in item title.</p>
     <form action="searchitemresults.php" method="get">
         <input type="text" name="terms" size="40" required />&nbsp;&nbsp;
-        <input type="submit" name="Submit" id="Submit" value="Search Item" />
+        <input type="submit" name="Submit" id="Submit" value="Search Item" class="search-back"/>
     </form>
 <?php
 include ('includes/footer.php');
