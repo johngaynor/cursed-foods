81fadd2e77875e8a23451aea4d08b1ba63142c7f
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-14 12:27:06 -0400

    Finished sortmenu.php

--- I210-Final/sortmenu.php
+++ I210-Final/sortmenu.php
@@ -1,9 +1,28 @@
 <?php
+// starting the session if it doesn't exist already
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-// if sort_by cannot be found, terminate script.
-if (!filter_has_var(INPUT_POST, 'sort_by')) {
+// retrieving session sort_by and storing it, if it doesn't exist creating an empty array
+if (isset($_SESSION['sort_by'])) {
+    $sort_by = $_SESSION['sort_by'];
+} else {
+    $sort_by = "";
+}
+
+// setting $sort_by. if sort_by cannot be found, terminate script.
+if(isset($_POST["sort_by"])){
+    $sort_method = $_POST["sort_by"];
+} else {
     $error = "Filter not found. Operation cannot proceed.<br><br>";
     header("Location: error.php?m=$error");
     die();
 }
+
+// updating the session variable
+$_SESSION['sort_by'] = $sort_method;
+
+header('Location: menu.php');
+?>
 
