c6e90bff005575580644aeeb6831bbbfa41a12b5
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-13 23:54:33 -0400

    changed the get variable

--- I210-Final/searchitemresults.php
+++ I210-Final/searchitemresults.php
@@ -1,3 +1,14 @@
 <?php
 
 // this is the file for searching items and returning results
+
+// include header and database
+include 'includes/header.php';
+include 'includes/database.php';
+
+// checking to see if any keywords exist in GET and handling errors accordingly
+if (!filter_has_var(INPUT_GET, 'terms')) {
+    echo "Error: no search terms were found.";
+    require_once ('includes/footer.php');
+    exit();
+}
--- I210-Final/searchitems.php
+++ I210-Final/searchitems.php
@@ -1,11 +1,11 @@
 <?php
 include ('includes/header.php');
 ?>
-    <h2>Search Books by Title</h2>
-    <p>Enter one or more words in book title.</p>
+    <h2>Search Items by Title</h2>
+    <p>Enter one or more words in item title.</p>
     <form action="searchitemresults.php" method="get">
         <input type="text" name="terms" size="40" required />&nbsp;&nbsp;
-        <input type="submit" name="Submit" id="Submit" value="Search Book" />
+        <input type="submit" name="Submit" id="Submit" value="Search Item" />
     </form>
 <?php
 include ('includes/footer.php');
