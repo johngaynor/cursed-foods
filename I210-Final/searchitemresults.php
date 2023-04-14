ed8e9db6dc0303c1403105db3347748a6fa57504
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-13 23:52:47 -0400

    created searchitemresults.php

---
+++ I210-Final/searchitemresults.php
@@ -0,0 +1,3 @@
+<?php
+
+// this is the file for searching items and returning results
--- I210-Final/searchitems.php
+++ I210-Final/searchitems.php
@@ -3,7 +3,7 @@ include ('includes/header.php');
 ?>
     <h2>Search Books by Title</h2>
     <p>Enter one or more words in book title.</p>
-    <form action="searchbookresults.php" method="get">
+    <form action="searchitemresults.php" method="get">
         <input type="text" name="terms" size="40" required />&nbsp;&nbsp;
         <input type="submit" name="Submit" id="Submit" value="Search Book" />
     </form>
