192573a6b6e76f9ff24440365c907a5da2b20ffa
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-15 02:12:54 -0400

    created login.php, starting to add functionality

---
+++ I210-Final/login.php
@@ -0,0 +1,13 @@
+<?php
+
+//include code from database.php
+require_once('includes/database.php');
+
+//start session if it has not already started
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+//create variable login status.
+$_SESSION['login_status'] = 2;
+$username = $passcode = "";
