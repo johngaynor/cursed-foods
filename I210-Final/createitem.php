9780119ca16f4fc984bc1420200132c518cabd78
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-16 16:25:00 -0400

    include database and retrieve inputs from form

--- I210-Final/createitem.php
+++ I210-Final/createitem.php
@@ -9,3 +9,12 @@ if(!filter_has_var(INPUT_POST, 'category_id') ||
     header("Location: error.php?m=$error");
     die();
 }
+
+//include code from header.php and database.php
+require_once('includes/database.php');
+
+//set all item details
+$firstname = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'category_id', FILTER_SANITIZE_NUMBER_INT)));
+$lastname = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'item_name', FILTER_UNSAFE_RAW)));
+$username = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'item_price', FILTER_SANITIZE_NUMBER_FLOAT)));
+$password = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'description', FILTER_UNSAFE_RAW)));
