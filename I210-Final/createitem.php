31e7fbd1013a3a984fb71c12b45da3be3b48929c
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-16 16:21:54 -0400

    retrieving proper form names

--- I210-Final/createitem.php
+++ I210-Final/createitem.php
@@ -1,11 +1,11 @@
 <?php
 
 //retrieve user inputs from the form
-if(!filter_has_var(INPUT_POST, 'firstname') ||
-    !filter_has_var(INPUT_POST, 'lastname') ||
-    !filter_has_var(INPUT_POST, 'username') ||
-    !filter_has_var(INPUT_POST, 'password')) {
-    $error = "The service is currently unavailable. Please try again later.";
+if(!filter_has_var(INPUT_POST, 'category_id') ||
+    !filter_has_var(INPUT_POST, 'item_name') ||
+    !filter_has_var(INPUT_POST, 'item_price') ||
+    !filter_has_var(INPUT_POST, 'description')) {
+    $error = "This service is currently unavailable. Please try again later.";
     header("Location: error.php?m=$error");
     die();
 }
