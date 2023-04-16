1d63a10315b3e74505d6a065e7b32e4ab83bd2d9
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-16 16:19:03 -0400

    retrieving item details from POST

--- I210-Final/createitem.php
+++ I210-Final/createitem.php
@@ -1,1 +1,11 @@
 <?php
+
+//retrieve user inputs from the form
+if(!filter_has_var(INPUT_POST, 'firstname') ||
+    !filter_has_var(INPUT_POST, 'lastname') ||
+    !filter_has_var(INPUT_POST, 'username') ||
+    !filter_has_var(INPUT_POST, 'password')) {
+    $error = "The service is currently unavailable. Please try again later.";
+    header("Location: error.php?m=$error");
+    die();
+}
