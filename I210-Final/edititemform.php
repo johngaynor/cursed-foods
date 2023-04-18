deb9e69ef2cc52bf7bc75193193cfa79affcac89
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-18 18:52:33 -0400

    added select for item category

--- I210-Final/edititemform.php
+++ I210-Final/edititemform.php
@@ -34,8 +34,9 @@ if (!$query) {
     exit;
 }
 
-$name = $category_name = $category_id = $price = $description = $image = '';
+$item_id = $name = $category_name = $category_id = $price = $description = $image = '';
 while ($row = $query->fetch_assoc()) {
+    $item_id = $row['item_id'];
     $name = $row['item_name'];
     $category_id = $row['category_id'];
     $category_name = $row['category_name'];
@@ -48,13 +49,31 @@ while ($row = $query->fetch_assoc()) {
 ?>
     <form action="edititem.php" method="post">
 <section class="details">
-
     <div class='item-desc'>
-        <input class="edit-name" id="detailHeader" type="text" name="name" value="<?= $name ?>" />
-        <p class='category' style='color: red'><span style='color: black'>Category: </span></p>
+        <input class="edit-name" id="detailHeader" type="text" name="name" value="<?= $name ?>" style="min-width: 500px" />
+        <p class='price'><span>Item ID: </span><input type="number" style="color: grey; width: 60px" readonly value="<?= $item_id ?>" /></p>
+        <p class='category' style='color: red'>
+            <span style='color: black'>Category: </span>
+            <select class='edit-select' name='category'>
+            <?php
+            // getting the categories to display in the select element
+            $sql2 = "SELECT * FROM categories";
+            $query2 = @$conn->query($sql2);
+
+            // looping through and displaying categories
+            while ($row = $query2->fetch_assoc()) {
+                if ($row['category_id'] == $category_id) {
+                    echo "<option style='color: red' value='", $row['category_id'], "' selected='selected'>", $row['category_name'], "</option>";
+                } else {
+                    echo "<option value='", $row['category_id'], "'>", $row['category_name'], "</option>";
+                }
+            }
+            ?>
+            </select>
+        </p>
         <p class='price'><span>Price: </span><input type="number" name="price" value="<?= $price ?>" /></p>
         <p class="desc"><span>Description: </span></p>
-        <textarea name="description" rows="6" cols="70"><?= $description ?></textarea>
+        <textarea name="description" rows="6" cols="70" style="margin-bottom: 60px"><?= $description ?></textarea>
         <div class="item-quantity">
                 <button class='addBtn' type='submit' style="margin-right: 10px">+ Save</button>
                 <button class="addBtn" type="button" onclick='window.location.href="menu-details.php?id=<?=$item_id?>"'>Cancel</button>
