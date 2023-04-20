c7dac204d79d3e577b5a1e15c6417f70a034d284
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-20 17:26:17 -0400

    Got the create item form sending things correctly to post

--- I210-Final/createitemform.php
+++ I210-Final/createitemform.php
@@ -33,25 +33,39 @@ if (isset($_SESSION['role'])) {
         <form action="createitem.php" method="post" class="add-product">
             <div class="add-holder">
                 <i class="fa-solid fa-pen"></i>
-                <input type="text" placeholder="Product Name">
+                <input type="text" placeholder="Product Name" name="item_name" required>
+            </div>
+            <div class="add-holder">
+                <i class="fa-solid fa-ghost"></i>
+                <select name="category_id" style="width: 100%; height: 50px" required>
+                    <option selected="selected" disabled>Select Category</option>
+                    <?php
+                    $sql = "SELECT * FROM categories";
+                    $query = @$conn->query($sql);
+                    while ($row = $query->fetch_assoc()) {
+                        echo "<option value='", $row['category_id'], "'>", $row['category_name'], "</option>";
+                    }
+                    // need to come back in and clean this up later
+                    ?>
+                </select>
             </div>
             <div class="prod-desc">
                 <i class="fa-solid fa-scroll"></i>
-                <textarea name="" id="" placeholder="Product Description"></textarea>
-            </div>
-            <div class="add-holder">
-                <i class="fa-solid fa-image"></i>
-                <input type="text" placeholder="Product Image" id="thumb-input" >
-                <button onclick='show_thumb()' type="button">Preview</button>
-                <img id="create-thumb" src=$img_thumb width="400px" />
+                <textarea name="description" placeholder="Product Description" required></textarea>
             </div>
             <div class="add-holder">
                 <i class="fa-solid fa-tags"></i>
-                <input type="text" placeholder="Product Price">
+                <input type="text" placeholder="Product Price" name="price" required>
+            </div>
+            <div class="add-holder">
+                <i class="fa-solid fa-image"></i>
+                <input type="text" placeholder="Product Image" id="thumb-input" name="image" required >
+<!--                <button onclick='show_thumb()' type="button">Preview</button>-->
+<!--                <img id="create-thumb" src=$img_thumb width="400px" />-->
             </div>
             <div class="prod-button-wrapper">
-                <button class="item-submit">Submit</button>
-                <button class="item-cancel">Cancel</button>
+                <button type="submit" class="item-submit">Submit</button>
+                <button type="button" class="item-submit" onclick="window.location.href ='menu.php'" >Cancel</button>
             </div>
         </form>
     </div>
