150326d4cad92378327fb5abd5fd9eae4f184b41
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-20 17:11:59 -0400

    got signup working

--- I210-Final/signup.php
+++ I210-Final/signup.php
@@ -1,11 +1,13 @@
 <?php
 
 //retrieve user inputs from the form
-if(!filter_has_var(INPUT_POST, 'firstname') ||
-    !filter_has_var(INPUT_POST, 'lastname') ||
-    !filter_has_var(INPUT_POST, 'username') ||
-    !filter_has_var(INPUT_POST, 'password')) {
-    $error = "The service is currently unavailable. Please try again later.";
+if(!filter_has_var(INPUT_POST, 'username') ||
+    !filter_has_var(INPUT_POST, 'fname') ||
+    !filter_has_var(INPUT_POST, 'lname') ||
+    !filter_has_var(INPUT_POST, 'email') ||
+    !filter_has_var(INPUT_POST, 'password') ||
+    !filter_has_var(INPUT_POST, 'profile_picture')) {
+    $error = "We are unable to create your account at this time. Please try again later.";
     header("Location: error.php?m=$error");
     die();
 }
@@ -14,17 +16,19 @@ if(!filter_has_var(INPUT_POST, 'firstname') ||
 //include code from header.php and database.php
 require_once('includes/database.php');
 
-$firstname = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'firstname', FILTER_UNSAFE_RAW)));
-$lastname = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'lastname', FILTER_UNSAFE_RAW)));
+$firstname = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'fname', FILTER_UNSAFE_RAW)));
+$lastname = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'lname', FILTER_UNSAFE_RAW)));
 $username = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'username', FILTER_UNSAFE_RAW)));
 $password = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'password', FILTER_UNSAFE_RAW)));
+$email = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'email', FILTER_UNSAFE_RAW)));
+$profile_picture = $conn->real_escape_string(trim(filter_input(INPUT_POST, 'profile_picture', FILTER_UNSAFE_RAW)));
 
 $role = 1;  //regular user
 
 //insert statement. The id field is an auto field.
-$sql = "INSERT INTO users VALUES (NULL, '$firstname', '$lastname', '$username', '$password', '$role')";
+$sql = "INSERT INTO users VALUES (NULL, '$username', '$firstname', '$lastname', NULL, '$email', '$password', '$role', '$profile_picture')";
 
-//execut the insert query
+//execute the insert query
 $query = @$conn->query($sql);
 
 //Handle insert errors
--- I210-Final/signupform.php
+++ I210-Final/signupform.php
@@ -7,25 +7,39 @@ include 'includes/database.php';
 <section class="create">
     <h1 class="createHeader">Create your account ;)</h1>
     <h2>Create your account to order food.</h2>
+    <form action="signup.php" method="post">
     <div class="create-form">
         <div class="login-inputs">
             <i class="fa-solid fa-user"></i>
-            <input type="text" placeholder="Username or Email" />
+            <input type="text" placeholder="Username" name="username" required />
+        </div>
+        <div class="login-inputs">
+            <i class="fa-solid fa-user"></i>
+            <input type="text" placeholder="First Name" name="fname" required />
+        </div>
+        <div class="login-inputs">
+            <i class="fa-solid fa-user"></i>
+            <input type="text" placeholder="Last Name" name="lname" required />
         </div>
         <div class="login-inputs">
             <i class="fa-solid fa-envelope"></i>
-            <input type="email" placeholder="Email" />
+            <input type="email" placeholder="Email" name="email" required />
         </div>
         <div class="login-inputs">
             <i class="fa-solid fa-lock"></i>
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" name="password" required />
         </div>
+<!--        <div class="login-inputs">-->
+<!--            <i class="fa-solid fa-calendar-days"></i>-->
+<!--            <input type="text" placeholder="Birthday" />-->
+<!--        </div>-->
         <div class="login-inputs">
-            <i class="fa-solid fa-calendar-days"></i>
-            <input type="text" placeholder="Birthday" />
+            <i class="fa-solid fa-image"></i>
+            <input type="text" placeholder="Profile Picture (url)" name="profile_picture" />
         </div>
-        <button class="signUpBtn">Sign Up</button>
+        <button class="signUpBtn" type="submit">Sign Up</button>
     </div>
+    </form>
 </section>
 
 <?php
