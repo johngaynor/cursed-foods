04c67cfeb4cae68226eb69dfe3ca64a7ddef4c00
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-21 20:25:02 -0400

    got the correct page loading

--- I210-Final/about.php
+++ I210-Final/about.php
@@ -3,29 +3,41 @@
 include 'includes/header.php';
 include 'includes/database.php';
 ?>
-    <section class="contact-form">
-        <h1>Get in Touch!</h1>
-        <form action="">
-            <div class="contact-holder">
-                <i class="fa-solid fa-user user-icon"></i>
-                <input type="text" placeholder="Full Name">
+
+    <section class="about">
+        <div class="about-hero">
+            <div class="about-hero-img">About Us</div>
+            <p class="about-hero-txt">Welcome to Cursed Foods, the website that brings you the most cursed and unsettling foods from around the world.
+                If you're brave enough to try these culinary monstrosities, you've come to the right place!</p>
+        </div>
+        <div class="about-section-wrapper">
+            <div class="about-section">
+                <div class="about-section-txt">
+                    <h2>Our Story</h2>
+                    <p>Our website was born out of a deep fascination with the weird and wonderful world of cursed foods. From bizarre flavor combinations to downright disturbing ingredients, we couldn't get enough of these culinary oddities. We decided to create a website that would showcase the best (or worst) of cursed foods, and share our passion with others who share our love of the bizarre.</p>
+                </div>
+                <img src="images/DALL·E%202023-04-18%2018.23%201.png" alt="">
             </div>
-            <div class="contact-holder">
-                <i class="fa-solid fa-envelope"></i>
-                <input type="email" placeholder="Email">
+            <div class="about-section">
+                <img src="images/DALL·E%202023-04-18%2018.23%202.png" alt="">
+                <div class="about-section-txt-mid">
+
+                    <h2>Our Mission</h2>
+                    <p>At Cursed Foods, our mission is to bring you the most unsettling and cringe-worthy foods from around the globe. We want to challenge your taste buds and push the boundaries of what you thought was possible in the realm of food. Our hope is that you'll come away from our website with a new appreciation for the weird and wonderful world of cursed cuisine.</p>
+                </div>
             </div>
-            <div class="contact-holder">
-                <i class="fa-solid fa-phone"></i>
-                <input type="text" placeholder="Phone Number">
+            <div class="about-section">
+                <div class="about-section-txt">
+                    <h2>What You Will Find!</h2>
+                    <p>On our website, you'll find a wide variety of cursed foods, from the mildly unsettling to the downright disturbing. We feature recipes, photos, and reviews of foods from all over the world, with a focus on the bizarre and the unexpected. Whether you're a fan of culinary curiosities or just looking for a good laugh, you're sure to find something to enjoy on Cursed Foods.</p>
+                </div>
+                <img src="images/DALL·E%202023-04-18%2018.23%203.png" alt="">
             </div>
-            <div class="comment-holder">
-                <i class="fa-solid fa-comment"></i>
-                <textarea placeholder="Comment"></textarea>
-            </div>
-        </form>
-        <div class="prod-button-wrapper">
-            <button class="item-submit">Submit</button>
-            <button class="item-cancel">Cancel</button>
+        </div>
+        <div class="about-contact">
+            <h2>Got a Question?</h2>
+            <p> Want to share your own cursed food creations with us? We'd love to hear from you! You can reach us by filling out our contact form below!</p>
+            <a href="contact.php">Contact Us</a>
         </div>
     </section>
 
