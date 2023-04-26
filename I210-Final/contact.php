bc62bfb3be1e2cc3adfa51d2804b3adc696aa672
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-04-25 23:21:31 -0400

    changed the contact form action

--- I210-Final/contact.php
+++ I210-Final/contact.php
@@ -7,28 +7,28 @@ include 'includes/database.php';
 
     <section class="contact-form">
         <h1>Get in Touch!</h1>
-        <form action="">
+        <form action="index.php">
             <div class="contact-holder">
                 <i class="fa-solid fa-user user-icon"></i>
-                <input type="text" placeholder="Full Name">
+                <input type="text" placeholder="Full Name" required>
             </div>
             <div class="contact-holder">
                 <i class="fa-solid fa-envelope"></i>
-                <input type="email" placeholder="Email">
+                <input type="email" placeholder="Email" required>
             </div>
             <div class="contact-holder">
                 <i class="fa-solid fa-phone"></i>
-                <input type="text" placeholder="Phone Number">
+                <input type="text" placeholder="Phone Number" required>
             </div>
             <div class="comment-holder">
                 <i class="fa-solid fa-comment"></i>
-                <textarea placeholder="Comment"></textarea>
+                <textarea placeholder="Comment" required></textarea>
+            </div>
+            <div class="prod-button-wrapper">
+                <button class="item-submit">Submit</button>
+                <button class="item-cancel" type="button" onclick="window.location.href='index.php'">Cancel</button>
             </div>
         </form>
-        <div class="prod-button-wrapper">
-            <button class="item-submit">Submit</button>
-            <button class="item-cancel">Cancel</button>
-        </div>
     </section>
 <?php
 include 'includes/footer.php';
